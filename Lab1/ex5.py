d727180adadba1e22386e2be41266d956663c367
Farah Najib <farahengnajib@gmail.com>
2024-03-26 13:56:33 +0100

    exercise5

---
+++ Lab1/ex5.py
@@ -0,0 +1,6 @@
+# age = 4 name = "Sammy"
+# output : "Hello my dog's name is Sammy and he is 4 years old"
+
+age = 4
+name = "Sammy"
+print("Hello my dog's name is {} and he is {} years old".format(name, age))
