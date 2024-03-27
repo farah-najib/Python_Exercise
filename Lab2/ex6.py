d57b80ffb7075cde87fd9e00a8630e69c4a8aebb
Farah Najib <farahengnajib@gmail.com>
2024-03-27 13:13:27 +0100

    exercise6

--- Lab2/ex6.py
+++ Lab2/ex6.py
@@ -1,1 +1,7 @@
 # Write a program that takes a string as input and prints its reverse.
+
+input_str = input()
+reverse_str = ""
+for i in input_str:
+    reverse_str = i + reverse_str
+print(reverse_str)
