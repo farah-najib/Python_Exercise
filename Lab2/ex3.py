55f3b57e0b3650638ce1a98ffa6aa2787695235e
Farah Najib <farahengnajib@gmail.com>
2024-03-27 12:41:34 +0100

    exercise4

--- Lab2/ex3.py
+++ Lab2/ex3.py
@@ -1,2 +1,9 @@
 # Write a program that creates a new tuple containing only the even numbers from a given tuple of integers.
 
+tuple_elements = (1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+new_tuple=()
+for element in tuple_elements :
+    if element%2 == 0 :
+      new_tuple+=(element,)
+
+print("new tuple:",new_tuple)
--- Lab2/ex4.py
+++ Lab2/ex4.py
@@ -1,2 +1,10 @@
 # Write a program that merges two dictionaries into a single dictionary. If a key is common to both dictionaries, the value from the second dictionary should be used.
 
+dictionary1 = {'a': 1, 'b': 2, 'c': 3, 'd': 5, 'e': 6}
+dictionary2 = {'b': 7,'f': 8, 'g': 9, 'h': 10}
+merged_dictionary=dictionary1
+for key, value in dictionary2.items():
+    merged_dictionary[key] = value
+print(dictionary1)
+print(dictionary2)
+print(merged_dictionary)
