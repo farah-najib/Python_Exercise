b669814654bae3740aef91c14c17a61cdc482971
Farah Najib <farahengnajib@gmail.com>
2024-03-27 11:54:26 +0100

    exercise1

--- Lab2/ex1.py
+++ Lab2/ex1.py
@@ -1,2 +1,10 @@
 # Write a program that takes two integers as input, base and exponent, and calculates the power using loops.
 
+base =  int(input("Enter base: "))
+exponent =  int(input("Enter exponent: "))
+reslut=1
+for i in range(1,exponent+1) :
+    reslut=reslut*base
+
+print(reslut)
+
