89995471c812284eaf43d69eb2a299e12d0107de
Farah Najib <farahengnajib@gmail.com>
2024-03-27 12:11:19 +0100

    exercise2

--- Lab2/ex2.py
+++ Lab2/ex2.py
@@ -1,2 +1,9 @@
 # Write a program that calculates the sum of all elements in a given tuple.
 
+tuple_elements = (1, 2, 4, 5, 6)
+result = 0
+for element in tuple_elements:
+        result += element
+
+
+print(result)
